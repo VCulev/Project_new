1df0bd59ff79a4d27bcfed85d917d771d51f56a8
Slavic Culev <slavicculev@Slavics-MacBook-Air.local>
2024-05-20 16:46:45 +0300

    added easter egg in about.html file

---
+++ frontend/js_files/easter_egg.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const easterEgg = document.getElementById('easterEgg');
+
+    easterEgg.addEventListener('click', () => {
+        window.location.href = 'https://www.sigmoidai.org';
+    });
+});
